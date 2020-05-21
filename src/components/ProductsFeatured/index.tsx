1e7a8113f4fba52770cc1529871dde033f33f642
Matt Jones <matthew.jones@chester.ac.uk>
2020-05-21 17:21:02 +0100

    Theme customisations

--- src/components/ProductsFeatured/index.tsx
+++ src/components/ProductsFeatured/index.tsx
@@ -47,7 +47,7 @@ const ProductsFeatured: React.FC<ProductsFeaturedProps> = ({ title }) => {
 };
 
 ProductsFeatured.defaultProps = {
-  title: "Featured",
+  title: "One bicycle. Many options. Carry every thing.",
 };
 
 export default ProductsFeatured;
